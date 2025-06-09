e01f2afeeb3e2823eb507d0111f634b509b2b8a7
unknown <findrayesa@gmail.com>
2025-06-09 13:33:15 +0700

    refactor: edit kmp.py

--- src/kmp.py
+++ src/kmp.py
@@ -3,6 +3,7 @@ import fitz
 import os
 
 def extract_pdf(path):
+    """Mengesktrak teks CV PDF menjadi string."""
     with open(path, "rb") as f:
         pdf_bytes = f.read()
 
@@ -18,8 +19,9 @@ def extract_pdf(path):
     return text, text_long
 
 def border(pattern):
-    b = [0 for _ in range(len(pattern))]
-    m = len(pattern)
+    """Membuat array fungsi border."""
+    b = [0 for _ in range(len(pattern)-1)]
+    m = len(pattern)-1
     j = 0
     i = 1
 
@@ -38,6 +40,7 @@ def border(pattern):
     return b
             
 def kmp(text, pattern):
+    """Implementasi algoritma KMP."""
     n = len(text)
     m = len(pattern)
 
@@ -63,77 +66,82 @@ def kmp(text, pattern):
         return counts
     return -1
  
-connect = sql.connect(
-    host="localhost",
-    user="root",
-    password="lagrange",
-    database="cv_application"
-)
+def main():
+    """Fungsi utama untuk menjalankan pencarian dengan algoritma KMP"""
+    connect = sql.connect(
+        host="localhost",
+        user="root",
+        password="lagrange",
+        database="cv_application"
+    )
 
-cursor = connect.cursor()
-cursor.execute("""
-               SELECT 
-                    ad.detail_id, ad.application_role, ad.cv_path,
-                    ap.first_name, ap.last_name, ap.date_of_birth, ap.address, ap.phone_number
-               FROM ApplicationDetail ad
-               JOIN ApplicantProfile ap ON ad.applicant_id = ap.applicant_id
-""")
+    cursor = connect.cursor()
+    cursor.execute("""
+                SELECT 
+                        ad.detail_id, ad.application_role, ad.cv_path,
+                        ap.first_name, ap.last_name, ap.date_of_birth, ap.address, ap.phone_number
+                FROM ApplicationDetail ad
+                JOIN ApplicantProfile ap ON ad.applicant_id = ap.applicant_id
+    """)
 
-keywords = ["javascript", "python"]
+    keywords = ["javascript", "python"]
 
-data_all = cursor.fetchall()
+    data_all = cursor.fetchall()
 
-found = {}
+    found = {}
 
-src_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.dirname(src_dir)
-data_dir = os.path.join(project_root, "data")
+    src_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.dirname(src_dir)
+    data_dir = os.path.join(project_root, "data")
 
-print("Mencari...")
-# exact matching
-for detailed_id, role, cv_path, fname, lname, birth, address, phone in data_all:
-    personal_info = f"{fname} {lname} {birth} {address} {phone}".lower()
+    print("Mencari...")
+    # exact matching
+    for detailed_id, role, cv_path, fname, lname, birth, address, phone in data_all:
+        personal_info = f"{fname} {lname} {birth} {address} {phone}".lower()
 
-    _, cv_text = extract_pdf(os.path.join(data_dir, cv_path))
+        _, cv_text = extract_pdf(os.path.join(data_dir, cv_path))
 
-    keyword_dict = {}
-    for kw in keywords:
-        found_in_personal = kmp(personal_info, kw.lower())
-        found_in_cv = kmp(cv_text, kw.lower())
+        keyword_dict = {}
+        for kw in keywords:
+            found_in_personal = kmp(personal_info, kw.lower())
+            found_in_cv = kmp(cv_text, kw.lower())
 
-        # Cek keberadaan keyword
-        if found_in_personal == -1 and found_in_cv != -1:
-            keyword_dict[kw] = found_in_cv
-        elif found_in_personal != -1 and found_in_cv == -1:
-            keyword_dict[kw] = found_in_personal 
-        elif found_in_personal != -1 and found_in_cv != -1:
-            keyword_dict[kw] = found_in_personal + found_in_cv
-        elif found_in_personal == -1 and found_in_cv == -1:
-            keyword_dict[kw] = 0
+            # Cek keberadaan keyword
+            if found_in_personal == -1 and found_in_cv != -1:
+                keyword_dict[kw] = found_in_cv
+            elif found_in_personal != -1 and found_in_cv == -1:
+                keyword_dict[kw] = found_in_personal 
+            elif found_in_personal != -1 and found_in_cv != -1:
+                keyword_dict[kw] = found_in_personal + found_in_cv
+            elif found_in_personal == -1 and found_in_cv == -1:
+                keyword_dict[kw] = 0
 
-    for val in keyword_dict.values():
-        if val != 0:
-            cv = cv_path.split("/")[-1]
-            found[cv] = keyword_dict
-            break
+        for val in keyword_dict.values():
+            if val != 0:
+                cv = cv_path.split("/")[-1]
+                found[cv] = keyword_dict
+                break
 
-# Panggil fungsi fuzzy matching
-# if found == {}:
+    # Panggil fungsi fuzzy matching
+    # if found == {}:
 
 
-# Mengurutkan hasil dari temuan terbanyak
-print("Hasil:")
-if found:
-    # Urutkan dict berdasarkan hasil temuan terbanyak
-    jumlah = {key: sum(list(val.values())) for key, val in found.items()}
-    jumlah_sorted = dict(sorted(jumlah.items(), key=lambda item: item[1], reverse=True))
-    found_sorted = {key: found[key] for key in jumlah_sorted.keys()}
+    # Mengurutkan hasil dari temuan terbanyak
+    print("Hasil:")
+    if found:
+        # Urutkan dict berdasarkan hasil temuan terbanyak
+        jumlah = {key: sum(list(val.values())) for key, val in found.items()}
+        jumlah_sorted = dict(sorted(jumlah.items(), key=lambda item: item[1], reverse=True))
+        found_sorted = {key: found[key] for key in jumlah_sorted.keys()}
 
-    print("Kata kunci ditemukan.")
-    print(found_sorted)
-    
-else:
-    print("Kata kunci tidak ditemukan.")
+        print("Kata kunci ditemukan.")
+        print(found_sorted)
+        
+    else:
+        print("Kata kunci tidak ditemukan.")
 
-cursor.close()
-connect.close()
+    cursor.close()
+    connect.close()
+
+if __name__ == "__main__":
+    main()
